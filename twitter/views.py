7216a5c59dfbc77c3d0c623653821b75b1530d32
Shintakumi <shincode0712@gmail.com>
2021-06-06 15:26:09 +0900

    modified dead.css and count

--- twitter/views.py
+++ twitter/views.py
@@ -15,8 +15,8 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 #friends_search_count = 200
-friends_ids_search_count = 5
-deadacount_definision = 30
+friends_ids_search_count = 15
+deadacount_definision = 60
 
 def homeView(request):
   return render(request, 'home.html')
